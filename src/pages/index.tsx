c042288b0526f19d879268a0bcdd37ffea322895
Flavia <flavia.bulad@gmail.com>
2023-01-29 20:59:34 -0300

    feat: :art: implement global style and cleanup index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import Head from "next/head";
-
-import { GlobalStyle } from "@/styles/globals";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>MKS Sistemas</title>
-        <meta name="description" content="MKS Front end Challenge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <GlobalStyle />
-      <main></main>
+      <main>
+        <h1>Hello World</h1>
+      </main>
     </>
   );
 }
