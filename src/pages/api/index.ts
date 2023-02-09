c15d1bbe2d8f82e4430fef40d3b02ea0aad10f03
Flavia <flavia.bulad@gmail.com>
2023-02-09 20:47:05 -0300

    feat: :sparkles: add api service

---
+++ src/pages/api/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+import { IApiParams } from "@/interfaces/api/IApiParams";
+import { IApiResponse } from "@/interfaces/api/IApiResponse";
+import { IProduct } from "@/interfaces/products/IProduct";
+
+const api = axios.create({
+  baseURL: "https://mks-frontend-challenge-api.herokuapp.com",
+});
+
+export default {
+  async getProducts(params: IApiParams): Promise<IProduct[] | undefined> {
+    try {
+      const response = await api.get<IApiResponse>("/api/v1/products", {
+        params,
+      });
+      return response.data.products;
+    } catch (err) {
+      console.error("Erro ao buscar produtos", err);
+    }
+  },
+};
